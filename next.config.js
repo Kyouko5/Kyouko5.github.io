143f2581f551f85e6b1f9f6a546bb90e261954ce
kani <kyouko@Kyoukos-MacBook-Air.local>
2025-04-18 00:56:51 +0800

    Remove basePath for user pages deployment

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/Kyouko5.github.io' : '', // 这里的 'portfolio' 需要替换成你的 GitHub 仓库名
   trailingSlash: true,
 }
 
