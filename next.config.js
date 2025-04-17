1cd5f39ed9fdca19ee78db1dfbfbce0a3f693f7d
kani <kyouko@Kyoukos-MacBook-Air.local>
2025-04-18 00:33:39 +0800

    Fix start script for static export

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
-/*
- * @Author: Kyouko
- * @Date: 2025-04-17 21:57:25
- * @LastEditTime: 2025-04-17 23:51:14
- * @Description: 请输入文件描述
- * @FilePath: /page/next.config.js
- */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export', // 启用静态导出
+  output: 'export',
   images: {
-    unoptimized: true, // 为了静态导出
-    domains: [],
+    unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '', // 替换为你的仓库名
   trailingSlash: true,
 }
 
