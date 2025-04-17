83325d0bfd7f4e7f98e2518c718884718979641d
kani <kyouko@Kyoukos-MacBook-Air.local>
2025-04-18 02:02:11 +0800

    add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,10 @@
+/*
+ * @Author: Kyouko
+ * @Date: 2025-04-17 21:57:57
+ * @LastEditTime: 2025-04-18 02:01:06
+ * @Description: 请输入文件描述
+ * @FilePath: /page/src/app/layout.tsx
+ */
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
@@ -15,6 +22,10 @@ export const metadata: Metadata = {
   authors: [{ name: "Kyouko" }],
   viewport: "width=device-width, initial-scale=1",
   robots: "index, follow",
+  icons: {
+    icon: '/favicon/black-cat.ico',
+    apple: '/favicon/christmas-decorations.png',
+  },
 };
 
 export default function RootLayout({
