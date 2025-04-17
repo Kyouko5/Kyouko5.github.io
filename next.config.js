74b0cd70e87760fd585823d4891dc42000d0c667
kani <kyouko@Kyoukos-MacBook-Air.local>
2025-04-17 23:56:04 +0800

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
+/*
+ * @Author: Kyouko
+ * @Date: 2025-04-17 21:57:25
+ * @LastEditTime: 2025-04-17 23:51:14
+ * @Description: 请输入文件描述
+ * @FilePath: /page/next.config.js
+ */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [], // 如果有外部图片域名，添加在这里
-  },
   output: 'export', // 启用静态导出
-  basePath: '/kyouko-portfolio', // 替换为你的 GitHub 仓库名
-  trailingSlash: true // 确保 URL 以 / 结尾，兼容 GitHub Pages
+  images: {
+    unoptimized: true, // 为了静态导出
+    domains: [],
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '', // 替换为你的仓库名
+  trailingSlash: true,
 }
 
 module.exports = nextConfig;
